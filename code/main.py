c06e8261d41a1492fac19939696c33cdc52bc81a
Pierre <pierre6richard7@gmail.com>
2024-11-06 10:11:55 +0100

    feat: right size camera

--- code/main.py
+++ code/main.py
@@ -4,10 +4,18 @@ from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.camera import Camera
 from kivy.core.window import Window
 
+# #-- maximize first, to get the screen size, minus any OS toolbars
+# Window.maximize()
+# maxSize = Window.system_size
+
+# #-- set the actual window size, to be slightly smaller than full screen
+# desiredSize = (maxSize[0]*0.9, maxSize[1]*0.9)
+# Window.size = desiredSize
+
 class TestCameraApp(App):
    def build(self):
       box=BoxLayout(orientation='vertical')
-      self.mycam=Camera(play=False)
+      self.mycam=Camera(play=False, index=1)
       box.add_widget(self.mycam)
       tb=ToggleButton(text='Play', size_hint_y= None, height= '48dp')
       tb.bind(on_press=self.play)
