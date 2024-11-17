3ab15e57dc06cfaca7a568bf584a9ceefb6d882b
Richard Pierre <76236062+Ranger1986@users.noreply.github.com>
2024-11-17 14:14:00 +0100

    clean comment LBPH.py

--- Code/LBPH.py
+++ Code/LBPH.py
@@ -4,12 +4,10 @@ import os
 
 def LBP_ORL(train_sample:int):
     def preprocess_image(img):
-        # Normaliser l'image pour améliorer le contraste
         return cv2.equalizeHist(img)
 
     faces = []
     ids = []
-    # Chargement des images et des IDs correspondants
     for s in range(1, 41):
         for i in range(1,train_sample+1):
             image_path = f"../Database/ORL/s{s}/{i}.pgm"
@@ -20,25 +18,12 @@ def LBP_ORL(train_sample:int):
                     faces.append(img)
                     ids.append(s)
     ids = np.array(ids, dtype='int32')
-    #print(len(faces))
-    # Initialiser le classificateur LBPH avec des paramètres ajustés
+    
     lbph_classifier = cv2.face.LBPHFaceRecognizer_create()
 
-    #print("before training")
     # Entraîner le modèle
     lbph_classifier.train(faces, ids)
-    #print("after training")
-    # Sauvegarder le modèle
-    # lbph_classifier.write('lbph_classifier.yml')
-    # print("after write")
 
-    # # Charger le modèle pour la prédiction
-    # lbph_face_classifier = cv2.face.LBPHFaceRecognizer_create()
-    # lbph_face_classifier.read('lbph_classifier.yml')
-    # print("after read")
-
-    # Tester le modèle
-    #print("before test")
     t = 0
     f = 0
     for s in range(1, 41):
@@ -51,9 +36,7 @@ def LBP_ORL(train_sample:int):
                     label, confidence = lbph_classifier.predict(img)
                     t += int(label == s)
                     f += int(label != s)
-                    #print(t+f)
-                    #print(f"Prédiction : {label}, Vérité : {s}, Confiance : {confidence}")
-    #print("after test")
+                    
     return (t,f)
 
 if __name__ == "__main__":
