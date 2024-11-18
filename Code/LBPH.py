f7d5db89e88a221c69f573f98d0807f7acfb75f1
Richard Pierre <76236062+Ranger1986@users.noreply.github.com>
2024-11-18 09:36:33 +0100

    LBPH on our database

--- Code/LBPH.py
+++ Code/LBPH.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import os
 
+def preprocess_image(img):
+    return cv2.equalizeHist(img)
+
 def LBP_ORL(train_sample:int):
-    def preprocess_image(img):
-        return cv2.equalizeHist(img)
 
     faces = []
     ids = []
@@ -39,7 +40,7 @@ def LBP_ORL(train_sample:int):
                     
     return (t,f)
 
-if __name__ == "__main__":
+def ORL():
     with open("log.txt", "w") as log_file:
         for i in range(1, 10):
             true_pred, false_pred = LBP_ORL(10 - i)
@@ -50,3 +51,43 @@ if __name__ == "__main__":
                 f"{true_pred} true, {false_pred} false for {total} predictions "
                 f"({accuracy:.2f}% accuracy)\n"
             )
+
+if __name__ == "__main__":
+    list_class = os.listdir("../Database/FaceExtracted")
+
+    faces = []
+    ids = []
+    for i in range(len(list_class)):
+        image_path = f"../Database/FaceExtracted/{list_class[i]}/"
+        list_img = os.listdir(image_path)
+        print(list_img)
+        for j in range(1):
+            if os.path.exists(image_path + list_img[0]):
+                img = cv2.imread(image_path + list_img[0], cv2.IMREAD_GRAYSCALE)
+                if img is not None:
+                    img = preprocess_image(img)
+                    faces.append(img)
+                    ids.append(i)
+    ids = np.array(ids, dtype='int32')
+    lbph_classifier = cv2.face.LBPHFaceRecognizer_create()
+
+    # Entraîner le modèle
+    lbph_classifier.train(faces, ids)
+
+    t = 0
+    f = 0
+    for i in range(len(list_class)):
+        image_path = f"../Database/FaceExtracted/{list_class[i]}/"
+        list_img = os.listdir(image_path)
+        print(list_img)
+        for j in range(2,4):
+            if os.path.exists(image_path + list_img[0]):
+                img = cv2.imread(image_path + list_img[0], cv2.IMREAD_GRAYSCALE)
+                if img is not None:
+                    img = preprocess_image(img)
+                    label, confidence = lbph_classifier.predict(img)
+                    t += int(label == i)
+                    f += int(label != i)
+                    
+    print("True: ", t, "\nFalse: ", f)
+                    
